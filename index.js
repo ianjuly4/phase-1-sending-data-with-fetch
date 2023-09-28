77c7999ddc5be6dd48d56f04ec3dd6d9d6767768
Ian July <ianjuly4@gmail.com>
2023-09-28 16:04:25 -0600

    sending data with fetch

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Add your code here
+function submitData(name = "name", email = "email"){
+  const dataSubmitted = {
+      name: `${name}`, 
+      email: `${email}`,
+  }
+  const configurationObject = {
+        method: "POST",
+        headers:{
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+            },
+            body: JSON.stringify(dataSubmitted),
+        }
+  return fetch("http://localhost:3000/users", configurationObject)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (response) {
+    return document.body.append(response.id)
+
+  })
+  .catch(function (error) {
+    return document.body.append(error);
+  });       
+}
+submitData("Ian", "ianjuly4@gmail.com")
